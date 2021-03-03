c9edf20b140d7679eb87fff92c1c1642c265c1bb
EduardoCG12 <eduardo.cosplayer@gmail.com>
2021-03-03 11:25:10 +0100

    Splash añadido

--- app/src/main/java/com/proyecto/gemcrush/SplashScreen.java
+++ app/src/main/java/com/proyecto/gemcrush/SplashScreen.java
@@ -2,7 +2,11 @@ package com.proyecto.gemcrush;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        TimerTask animado = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(animado, 4000);
     }
 }
