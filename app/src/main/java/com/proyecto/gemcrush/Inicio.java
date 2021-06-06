59e36dc7af8925062847d357eee83f19f35f1c84
4rkaitz <arkaitz.garot@gmail.com>
2021-06-06 18:46:53 +0200

    Añadida transición entre el SplashScreen y el inicio y añadido fondo al SplashScreen.

--- app/src/main/java/com/proyecto/gemcrush/Inicio.java
+++ app/src/main/java/com/proyecto/gemcrush/Inicio.java
@@ -1,23 +1,15 @@
 package com.proyecto.gemcrush;
 
-import android.annotation.SuppressLint;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.DateFormat;
